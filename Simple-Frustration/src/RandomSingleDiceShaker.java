da5983bc69ad8e66b98dfd5d74f3d3f4d50f684d
Tom Davidson <tldavidson2004@gmail.com>
2025-02-21 12:51:46 +0000

    Updated changes

---
+++ Simple-Frustration/src/RandomSingleDiceShaker.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class RandomSingleDiceShaker implements DiceShaker {
+    private final Random random = new Random();
+
+    @Override
+    public int shake() {
+        return random.nextInt(6) + 1;
+    }
+}
