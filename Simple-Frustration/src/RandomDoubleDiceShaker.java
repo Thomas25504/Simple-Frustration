119f3d8d7b1ce90f8784c3ff0e6b95946f0d12c6
Tom Davidson <tldavidson2004@gmail.com>
2025-02-28 18:51:09 +0000

    Add changes

---
+++ Simple-Frustration/src/RandomDoubleDiceShaker.java
@@ -0,0 +1,8 @@
+public class RandomDoubleDiceShaker implements DiceShaker {
+    private final DiceShaker shaker = new RandomSingleDiceShaker();
+
+    @Override
+    public int shake(){
+        return shaker.shake() + shaker.shake();
+    }
+}
